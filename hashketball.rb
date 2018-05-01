1faacef51882625bc160432caa73616d11b596fe
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-01 02:35:17 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def game_hash
-game_hash =
+game_hash = {
   :home => {:team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => {
@@ -103,6 +103,7 @@ game_hash =
         }
       },
     }
+  }
 end
 
 def good_practices
